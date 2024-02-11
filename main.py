47c4b254fd2149bd760863f68fef8e3d37a49fd3
Ike <ike@magicx.ai>
2024-02-12 02:54:51 +0900

    basic relative time

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import logging
 import os
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+import regex
 from regex import time_re_pattern
 
 NAME = "slack-tz-helper"
@@ -31,7 +32,7 @@ def send_ephemeral_message_to_channel_members(sender_id, channel_id, trigger_mes
             for original_time, utc_time in time_to_utc_dic.items():
                 receiver_tz = ZoneInfo(get_user_timezone(user_id))
                 t = utc_time.astimezone(receiver_tz)
-                msg = msg.replace(original_time, f"*`{t.strftime('%H:%M %p, %Y-%m-%d')}`*[{receiver_tz}]")
+                msg = msg.replace(original_time, f"*`{t.strftime('%I:%M %p, %Y-%m-%d')}`*[{receiver_tz}]")
 
             # send "only visible to you" message for each user in the channel
             client.chat_postEphemeral(
@@ -41,6 +42,7 @@ def send_ephemeral_message_to_channel_members(sender_id, channel_id, trigger_mes
             )
     except Exception as e:
         logger.error(f"Error: {e}")
+        raise e
 
 
 def get_user_timezone(user_id):
@@ -64,7 +66,7 @@ def extract_utc_from_time(time_list, sender_tz):
 
     # python logic that extract utc from time like "10:30 PM" with sender's timezone("Asia/Seoul")
     sender_tz = "America/Los_Angeles" # TODO for testing
-    input_format = '%H:%M%p'
+    input_format = '%I:%M%p'
     for time in time_list:
         processed_time = postprocess_time(time)
         t = datetime.strptime(processed_time, input_format)
@@ -81,17 +83,35 @@ def extract_utc_from_time(time_list, sender_tz):
 
     return time_to_utc_dic
 
+def relative_date_to_timedelta(today, rel_date: str):
+    timedelta_dic = {
+        "today": 0,
+        "td": 0,
+        "tomorrow": 1,
+        "tmr": 1,
+        "yesterday": -1,
+        "yd": -1,
+        # TODO next, last
+    }
+    return timedelta(days=timedelta_dic[rel_date])
+
 
 @bolt.message(time_re_pattern)
 def timezone_convert(message, context):
     sender_id = message['user']
-    original_times = [time for time in context['matches']]
     sender_tz = get_user_timezone(sender_id)
+    msg = message['text']
+
+    original_times = [time for time in context['matches']]
     time_to_utc_dic = extract_utc_from_time(original_times, sender_tz)
 
+    rel_date = regex.date_re_pattern.search(msg)[0].strip()  # use only the first match
+    timedelta = relative_date_to_timedelta(datetime.now(ZoneInfo(sender_tz)), rel_date)
+    time_to_utc_dic = {k: v + timedelta for k, v in time_to_utc_dic.items()}
+
     sender_tz = "America/Los_Angeles" # TODO for testing
-    suffix = f"\n--FROM user[{sender_id}] timezone *{sender_tz}*\n>{message['text']}"
-    send_ephemeral_message_to_channel_members(sender_id, message['channel'], message['text'], time_to_utc_dic, suffix)
+    suffix = f"\n--FROM user[{sender_id}] timezone *{sender_tz}*\n>{msg}"
+    send_ephemeral_message_to_channel_members(sender_id, message['channel'], msg, time_to_utc_dic, suffix)
 
 
 if __name__ == "__main__":
--- regex.py
+++ regex.py
@@ -6,8 +6,8 @@ time_re_pattern = re.compile(r'\s(\d{1,2}\:\d{1,2}\s?(?:AM|PM|am|pm))')
 
 # case 2 ex - 10 PM
 
-
-# date related
-
-# timezone related
-timezone_re_pattern = re.compile(r'pst|PST|pdt|PDT|est|EST|edt|EDT|cst|CST|cdt|CDT|utc|UTC|gmt|GMT|kst|KST|jst|JST|cet|CET')
+date_re_pattern = re.compile(r'\s?(tomorrow|Tomorrow|tmr|Tmr|'
+                             r'today|Today|td|Td|'
+                             r'yesterday|Yesterday|yd|Yd)\s?')
+                             # r'next|'
+                             # r'last')
