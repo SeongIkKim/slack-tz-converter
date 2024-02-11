52754abd7d1f0519444ee07fa8a244c8ead8277b
Ike <ike@magicx.ai>
2024-02-12 01:28:58 +0900

    basic timezone parsing

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import logging
 import os
 import re
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from regex import time_re_pattern
 
 NAME = "slack-tz-helper"
 logger = logging.getLogger(NAME)
@@ -32,11 +36,59 @@ def send_ephemeral_message_to_channel_members(channel_id, trigger_message):
         logger.error(f"Error: {e}")
 
 
-@bolt.message("6am")
+def get_user_timezone(user_id):
+    try:
+        result = client.users_info(user=user_id)
+        user = result['user']
+        tz_label = user['tz']
+        return tz_label
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        raise e
+
+
+def postprocess_time(time_list):
+    # "10:30 PM" -> "10:30PM"
+    time_list = [time.replace(" ", "") for time in time_list]
+    return time_list
+
+
+def extract_utc_from_time(time_list, sender_tz):
+    time_to_utc_dic = {}
+
+    # python logic that extract utc from time like "10:30 PM" with sender's timezone("Asia/Seoul")
+    sender_tz = "America/Los_Angeles"
+    input_format = '%H:%M%p'
+    for time in time_list:
+        t = datetime.strptime(time, input_format)
+        # date가 안들어왔다는 가정 하에
+        date_now = datetime.now(ZoneInfo(sender_tz))
+        local_time_with_tz = t.replace(
+            year=date_now.year,
+            month=date_now.month,
+            day=date_now.day,
+            tzinfo=ZoneInfo(sender_tz)
+        )
+        utc_time = local_time_with_tz.astimezone(ZoneInfo("UTC"))
+        time_to_utc_dic[time] = utc_time
+
+    return time_to_utc_dic
+
+
+@bolt.message(time_re_pattern)
 def timezone_convert(message, context):
-    user = message['user']
+    sender_id = message['user']
+    original_times = [time for time in context['matches']]
+    sender_tz = get_user_timezone(sender_id)
+    original_times = postprocess_time(original_times)
+    time_to_utc_dic = extract_utc_from_time(original_times, sender_tz)
+
+    print(time_to_utc_dic)
+
+    # 각 매칭된 시간에 대해 "original time" -> "UTC time" 딕셔너리를 만들어서 넘김.
     time = context['matches'][0]
-    send_ephemeral_message_to_channel_members(message['channel'], f"timezone-related word detected, user - {user} - {time}")
+
+    send_ephemeral_message_to_channel_members(message['channel'], f"{sender_id} - {time}")
 
 
 if __name__ == "__main__":
--- regex.py
+++ regex.py
@@ -1,2 +1,13 @@
+import re
 
-# timezone_related_pattern
+# time related
+# case 1 - 10:30 PM
+time_re_pattern = re.compile(r'\s(\d{1,2}\:\d{1,2}\s?(?:AM|PM|am|pm))')
+
+# case 2 ex - 10 PM
+
+
+# date related
+
+# timezone related
+timezone_re_pattern = re.compile(r'pst|PST|pdt|PDT|est|EST|edt|EDT|cst|CST|cdt|CDT|utc|UTC|gmt|GMT|kst|KST|jst|JST|cet|CET')
