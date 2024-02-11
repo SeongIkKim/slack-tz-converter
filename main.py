4f6949e4ac0ddaeb6c6223fce5fefc9a4b99282c
Ike <ike@magicx.ai>
2024-02-12 04:40:59 +0900

    fix

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import logging
 import os
-import re
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
+import regex
+from regex import time_re_pattern
 
 from slack_sdk import WebClient
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-import regex
-from regex import time_re_pattern
+from dateutil.relativedelta import relativedelta, FR, MO, TU, WE, TH, SA, SU
 
 NAME = "slack-tz-helper"
 logger = logging.getLogger(NAME)
@@ -28,44 +27,54 @@ def send_ephemeral_message_to_channel_members(sender_id, channel_id, trigger_mes
             if user_id in {bolt.client.auth_test()["user_id"]}:
                 continue
 
+            receiver_tz = ZoneInfo(get_user_timezone(get_user_info(user_id)))
+
             msg = trigger_message
             for original_time, utc_time in time_to_utc_dic.items():
-                receiver_tz = ZoneInfo(get_user_timezone(user_id))
                 t = utc_time.astimezone(receiver_tz)
-                msg = msg.replace(original_time, f"*`{t.strftime('%I:%M %p, %Y-%m-%d')}`*[{receiver_tz}]")
+                msg = msg.replace(original_time, f"*{t.strftime('%I:%M %p, %a, %Y-%m-%d')}*")
 
             # send "only visible to you" message for each user in the channel
             client.chat_postEphemeral(
                 channel=channel_id,
                 user=user_id,
-                text=f"{msg} {suffix}"
+                text=f"{msg} {suffix} to `{receiver_tz}`"
             )
     except Exception as e:
         logger.error(f"Error: {e}")
         raise e
 
 
-def get_user_timezone(user_id):
+def get_user_info(user_id):
     try:
         result = client.users_info(user=user_id)
         user = result['user']
-        tz_label = user['tz']
-        return tz_label
+        return user
     except Exception as e:
         logger.error(f"Error: {e}")
         raise e
 
+def get_user_timezone(user_info):
+    return user_info.get('tz')
+
+def get_user_name(user_info):
+    return user_info.get('name')
 
 def postprocess_time(time):
+    # "10PM" -> "10:00PM"
+    if ':' not in time:
+        time = f"{time[:-2]}:00{time[-2:]}"
     # "10:30 PM" -> "10:30PM"
-    return time.strip().replace(" ", "")
+    time = time.strip().replace(" ", "")
+    return time
+
+
 
 
 def extract_utc_from_time(time_list, sender_tz):
     time_to_utc_dic = {}
 
     # python logic that extract utc from time like "10:30 PM" with sender's timezone("Asia/Seoul")
-    sender_tz = "America/Los_Angeles" # TODO for testing
     input_format = '%I:%M%p'
     for time in time_list:
         processed_time = postprocess_time(time)
@@ -83,35 +92,84 @@ def extract_utc_from_time(time_list, sender_tz):
 
     return time_to_utc_dic
 
-def relative_date_to_timedelta(today, rel_date: str):
-    timedelta_dic = {
+def relative_date_to_timedelta(rel_date: str, weekday: str = None):
+    day_timedelta_dic = {
         "today": 0,
         "td": 0,
+        "tonight": 0,
         "tomorrow": 1,
         "tmr": 1,
         "yesterday": -1,
         "yd": -1,
-        # TODO next, last
+        # TODO next, last, this
     }
-    return timedelta(days=timedelta_dic[rel_date])
+
+    week_timedelta_dic = {
+        "this": 0,
+        "next": 1,
+        "last": -1,
+    }
+
+    weekday_dic = {
+        "monday": MO,
+        "tuesday": TU,
+        "wednesday": WE,
+        "thursday": TH,
+        "friday": FR,
+        "saturday": SA,
+        "sunday": SU,
+    }
+
+    if rel_date.lower() in day_timedelta_dic:
+        return timedelta(days=day_timedelta_dic[rel_date])
+
+    if rel_date.lower() in week_timedelta_dic and weekday is not None:
+        rel_weekday = weekday_dic[weekday.lower()](week_timedelta_dic[rel_date.lower()])
+        return relativedelta(weekday=rel_weekday)
+
 
 
 @bolt.message(time_re_pattern)
 def timezone_convert(message, context):
+    start_time = datetime.now()
+
     sender_id = message['user']
-    sender_tz = get_user_timezone(sender_id)
-    msg = message['text']
+    sender_info = get_user_info(sender_id)
+    sender_name = get_user_name(sender_info)
+    sender_tz = get_user_timezone(sender_info)
+    sender_tz = "Asia/Shanghai"  # TODO for testing
+    original_msg = message['text']
+    converted_msg = original_msg
+
+    time_1 = datetime.now()
+    print(f"Getting user info: {time_1 - start_time}")
 
     original_times = [time for time in context['matches']]
     time_to_utc_dic = extract_utc_from_time(original_times, sender_tz)
 
-    rel_date = regex.date_re_pattern.search(msg)[0].strip()  # use only the first match
-    timedelta = relative_date_to_timedelta(datetime.now(ZoneInfo(sender_tz)), rel_date)
-    time_to_utc_dic = {k: v + timedelta for k, v in time_to_utc_dic.items()}
+    time_2 = datetime.now()
+    print(f"extract utc from info: {time_2 - time_1}")
 
-    sender_tz = "America/Los_Angeles" # TODO for testing
-    suffix = f"\n--FROM user[{sender_id}] timezone *{sender_tz}*\n>{msg}"
-    send_ephemeral_message_to_channel_members(sender_id, message['channel'], msg, time_to_utc_dic, suffix)
+    rel_date_match = regex.date_re_pattern.search(original_msg)
+    rel_date = rel_date_match[0].strip() if rel_date_match and rel_date_match[0] else None # use only the first match
+    weekday_match = regex.weekday_re_pattern.search(original_msg)
+    weekday = weekday_match[0].strip() if weekday_match and weekday_match[0] else None  # use only the first match
+    if rel_date:
+        timedelta = relative_date_to_timedelta(rel_date, weekday)
+        time_to_utc_dic = {k: v + timedelta for k, v in time_to_utc_dic.items()}
+        converted_msg = converted_msg.replace(rel_date, f"~{rel_date}~")
+        if weekday:
+            converted_msg = converted_msg.replace(weekday, f"~{weekday}~")
+
+    time_3 = datetime.now()
+    print(f"relative date to timedelta: {time_3 - time_2}")
+
+    suffix = f"\n>{sender_name} : _{original_msg}_\nconverted from `{sender_tz}`"
+    send_ephemeral_message_to_channel_members(sender_id, message['channel'], converted_msg, time_to_utc_dic, suffix)
+
+    time_4 = datetime.now()
+    print(f"send ephemeral message: {time_4 - time_3}")
+    print(f"total time: {time_4 - start_time}")
 
 
 if __name__ == "__main__":
--- regex.py
+++ regex.py
@@ -2,12 +2,21 @@ import re
 
 # time related
 # case 1 - 10:30 PM
-time_re_pattern = re.compile(r'\s(\d{1,2}\:\d{1,2}\s?(?:AM|PM|am|pm))')
+time_re_pattern = re.compile(r'\s(\d{1,2}(?::\d{2})?\s?(?:AM|PM|am|pm))') # 09:00 PM, 9:00pm, 9 PM, 9 am, etc
 
 # case 2 ex - 10 PM
 
-date_re_pattern = re.compile(r'\s?(tomorrow|Tomorrow|tmr|Tmr|'
-                             r'today|Today|td|Td|'
-                             r'yesterday|Yesterday|yd|Yd)\s?')
-                             # r'next|'
-                             # r'last')
+date_re_pattern = re.compile(r'\b(tomorrow|Tomorrow|tmr|Tmr|'
+                             r'today|Today|td|Td|tonight|Tonight|'
+                             r'yesterday|Yesterday|yd|Yd|'
+                             r'next|Next|'
+                             r'last|Last|'
+                             r'this|This)\b')
+
+weekday_re_pattern = re.compile(r'\b(monday|Monday|'
+                                r'tuesday|Tuesday|'
+                                r'wednesday|Wednesday|'
+                                r'thursday|Thursday|'
+                                r'friday|Friday|'
+                                r'saturday|Saturday|'
+                                r'sunday|Sunday)\b')
